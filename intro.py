3ea9c59687fb36a7b7683223e819511a9e6f375d
cwaruinu01 <cwaruinu01@gmail.com>
2024-03-12 08:24:56 +0300

    python project

---
+++ intro.py
@@ -0,0 +1,18 @@
+#learning variables
+x=3
+y="Betty"
+print(x)
+print(y)
+
+#
+#casting
+
+#casing
+#camel case
+studentNameKenya="Ken"
+
+#snake case
+student_name_kenya="Ken"
+
+
+print(student_name_kenya)
